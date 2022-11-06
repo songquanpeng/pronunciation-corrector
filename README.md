d55a2106a3d2fdb876175be4c904e8d7ee9a066e
JustSong <songquanpeng@foxmail.com>
2022-11-06 11:32:50 +0800

    :tada: initialize v0.2

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # 在线英语发音纠正
 > 拯救你的英语发音，告别因发音错误带来的尴尬！
 
+<p>
+  <a href="https://raw.githubusercontent.com/songquanpeng/pronunciation-corrector/main/LICENSE">
+    <img src="https://img.shields.io/github/license/songquanpeng/pronunciation-corrector?color=brightgreen" alt="license">
+  </a>
+  <a href="https://github.com/songquanpeng/pronunciation-corrector/releases/latest">
+    <img src="https://img.shields.io/github/v/release/songquanpeng/pronunciation-corrector?color=brightgreen&include_prereleases" alt="release">
+  </a>
+  <a href="https://github.com/songquanpeng/pronunciation-corrector/releases/latest">
+    <img src="https://img.shields.io/github/downloads/songquanpeng/pronunciation-corrector/total?color=brightgreen&include_prereleases" alt="release">
+  </a>
+  <a href="https://goreportcard.com/report/github.com/songquanpeng/go-file">
+    <img src="https://goreportcard.com/badge/github.com/songquanpeng/pronunciation-corrector" alt="GoReportCard">
+  </a>
+</p>
+
 即刻开始练习：https://corrector.justsong.cn/
 
 ## 功能
 + [x] 在线发音校正。
 + [x] 支持选择词库。
 + [x] 支持保存进度。
-
++ [ ] 显示音标以及翻译。
++ [ ] 跨设备设置与进度同步。
++ [ ] 支持自定义词库。
++ [ ] 支持分享词库。
 
 ## 已支持的词库
 ```json
@@ -21,5 +39,6 @@
 ```
 
 ## 其他
-1. 由 [jingjingxyk](https://github.com/jingjingxyk/) 开发的[配套 Chrome 扩展](https://github.com/jingjingxyk/pronunciation-corrector-chromium-extension)。
-2. 《中国程序员容易发音错误的单词》[词库来源](https://github.com/shimohq/chinese-programmer-wrong-pronunciation)。
+1. [v0.1 版本代码](https://github.com/songquanpeng/pronunciation-corrector/tree/v1)，后续版本代码基于 [Gin Template](https://github.com/songquanpeng/gin-template)。
+2. 由 [jingjingxyk](https://github.com/jingjingxyk/) 开发的[配套 Chrome 扩展](https://github.com/jingjingxyk/pronunciation-corrector-chromium-extension)（目前仅适配了 v0.1 版本）。
+3. 《中国程序员容易发音错误的单词》[词库来源](https://github.com/shimohq/chinese-programmer-wrong-pronunciation)。
---
+++ controller/github.go
@@ -0,0 +1,152 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"pronunciation-corrector/common"
+	"pronunciation-corrector/model"
+	"time"
+)
+
+type GitHubOAuthResponse struct {
+	AccessToken string `json:"access_token"`
+	Scope       string `json:"scope"`
+	TokenType   string `json:"token_type"`
+}
+
+type GitHubUser struct {
+	Login string `json:"login"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func GitHubOAuth(c *gin.Context) {
+	if !common.GitHubOAuthEnabled {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "管理员未开启通过 GitHub 登录以及注册",
+		})
+		return
+	}
+	code := c.Query("code")
+	if code == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "非法的参数",
+		})
+		return
+	}
+	values := map[string]string{"client_id": common.GitHubClientId, "client_secret": common.GitHubClientSecret, "code": code}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	req, err := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(jsonData))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	defer res.Body.Close()
+	var oAuthResponse GitHubOAuthResponse
+	err = json.NewDecoder(res.Body).Decode(&oAuthResponse)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	req, err = http.NewRequest("GET", "https://api.github.com/user", nil)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", oAuthResponse.AccessToken))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	res2, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	defer res2.Body.Close()
+	var githubUser GitHubUser
+	err = json.NewDecoder(res2.Body).Decode(&githubUser)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	if githubUser.Login == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "返回值非法，用户字段为空",
+		})
+		return
+	}
+	user := model.User{
+		GitHubId: githubUser.Login,
+	}
+	if model.IsGitHubIdAlreadyTaken(user.GitHubId) {
+		user.FillUserByGitHubId()
+	} else {
+		user.Username = "github_" + common.GetUUID()
+		user.DisplayName = githubUser.Name
+		user.Email = githubUser.Email
+		user.Role = common.RoleCommonUser
+		user.Status = common.UserStatusEnabled
+
+		if err := user.Insert(); err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}
+
+	if user.Status != common.UserStatusEnabled {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "用户已被封禁",
+			"success": false,
+		})
+		return
+	}
+	setupLogin(&user, c)
+}
